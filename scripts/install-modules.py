3063b2b9ed66cb5013a198b2ccb1acb35a40bcc2
Junjun Zhang <junjun.ca@gmail.com>
2020-01-17 16:19:55 -0500

    mkdirs if not exist

--- scripts/install-modules.py
+++ scripts/install-modules.py
@@ -52,6 +52,7 @@ def install_modules(remote_module_paths):
     except:
       sys.exit('Error: unable to fetch remote module file at: %s' % url)
 
+    os.makedirs(os.path.dirname(local_path), exist_ok=True)
     open(local_path, 'wb').write(r.content)
 
 
